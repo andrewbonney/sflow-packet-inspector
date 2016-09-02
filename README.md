788cc1edf26f67a041c7bbf95086905f81208950
Andrew Bonney <andrew.bonney@bbc.co.uk>
2016-09-02 09:29:47 +0100

    inspector: update with support for first stage of tests

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ This is intended for operation on small private networks.
 
 ## Issues Detected
 
+* Sending to an incorrect MAC address for a given multicast IP
+* Sending to or from an all 0x00 or all 0xff MAC address
+
+## TODO
+
 * Sending high volume multicast to groups not in the 232 or 239 ranges
-* Sending to a non-multicast MAC but with a multicast IP
-* Sending to the wrong multicast MAC for a given multicast IP
-* Sending to or from a zero MAC address, or a common testing MAC
 
 Further issue detection will be added at a later date.
--- inspector.py
+++ inspector.py
@@ -7,48 +7,125 @@ import time
 import thread
 import traceback
 
-SFLOW_INTERVAL=4000
 HTTP_PORT=8000
+
+SWITCHES={}
 
 class sFlowSample():
     def __init__(self, line):
-        self.src_mac = None
-        self.dst_mac = None
-        self.src_ip = None
-        self.dst_ip = None
+        line_bits = line.split(",")
+        sample_def = [
+            "sw_agn_ip",
+            "sw_in_port",
+            "sw_out_port",
+            "src_mac",
+            "dst_mac",
+            "eth_type",
+            "in_vlan",
+            "out_vlan",
+            "src_ip",
+            "dst_ip",
+            "ip_proto",
+            "ip_tos",
+            "ip_ttl",
+            "src_port",
+            "dst port",
+            "tcp_flags",
+            "pkt_size",
+            "ip_pkt_size",
+            "sample_rate"
+        ]
+
+        self.sflow_data = {}
+        index = 1
+        for entry in sample_def:
+            self.sflow_data[entry] = line_bits[index]
+            index += 1
+
+        if self.sflow_data["sw_agn_ip"] in SWITCHES:
+            SWITCHES[self.sflow_data["sw_agn_ip"]]["sample_rate"] = self.sflow_data["sample_rate"]
+
+    def __getattr__(self, attr):
+        return self.sflow_data[attr]
+
+
+class sFlowCounter():
+    def __init__(self, line):
+        line_bits = line.split(",")
+        counter_def = [
+            "sw_agn_ip",
+            "if_index",
+            "if_type",
+            "if_speed",
+            "if_direction",
+            "if_in_octets",
+            "if_in_ucast_pkts",
+            "if_in_mcast_pkts",
+            "if_in_bcast_pkts",
+            "if_in_discards",
+            "if_in_errors",
+            "if_in_unknown_protos",
+            "if_out_octets",
+            "if_out_ucast_pkts",
+            "if_out_mcast_pkts",
+            "if_out_bcast_pkts",
+            "if_out_discards",
+            "if_out_errors",
+            "if_promiscuous_mode"
+        ]
+
+        self.sflow_data = {}
+        index = 1
+        for entry in counter_def:
+            self.sflow_data[entry] = line_bits[index]
+            index += 1
+
+        if self.sw_agn_ip not in SWITCHES:
+            SWITCHES[self.sflow_data["sw_agn_ip"]] = {"interfaces": [], "sample_rate": 4000}
+        while len(SWITCHES[self.sflow_data["sw_agn_ip"]]["interfaces"]) < (int(self.sflow_data["if_index"]) + 1):
+            SWITCHES[self.sflow_data["sw_agn_ip"]]["interfaces"].append({})
+
+        SWITCHES[self.sflow_data["sw_agn_ip"]]["interfaces"][int(self.sflow_data["if_index"])] = self.sflow_data
+
+    def __getattr__(self, attr):
+        return self.sflow_data[attr]
 
 
 class sFlowTestResult():
-    def __init__(self, error_present, error_msg=None):
+    def __init__(self, error_present, error_msg=None, sample=None):
         self.error_present = error_present
         self.error_msg = error_msg
+        self.sample = sample
 
     def isError(self):
         return self.error_present
 
     def getMessage(self):
-        return self.error_msg
+        if self.sample is not None:
+            return self.error_msg + "\n- {} {}->{} {}->{} {}->{}".format(self.sample.sw_agn_ip, self.sample.sw_in_port, self.sample.sw_out_port, self.sample.src_mac, self.sample.dst_mac, self.sample.src_ip, self.sample.dst_ip)
+        else:
+            return self.error_msg
 
 
 class sFlowTests():
     def testBadMAC(self, sample):
-        BAD_MACS = ["00:00:00:00:00:00", "ff:ff:ff:ff:ff:ff"]
+        BAD_MACS = ["000000000000", "ffffffffffff"]
         if sample.src_mac in BAD_MACS:
-            return sFlowTestResult(True, "Src MAC is invalid ({})".format(sample.src_mac))
+            return sFlowTestResult(True, "Src MAC is invalid", sample)
         elif sample.dst_mac in BAD_MACS:
-            return sFlowTestResult(True, "Dest MAC is invalid ({})".format(sample.dst_mac))
-        
+            return sFlowTestResult(True, "Dest MAC is invalid", sample)
+
         return sFlowTestResult(False)
 
     def testIncorrectMulticastMAC(self, sample):
-        if sample.dst_ip[0] >= 224 or sample.dst_ip[0] <= 239:
-            if self.dst_mac[0:8] != "01:00:5e":
-                return sFlowTestResult(True, "Dest MAC is not a valid multicast MAC ({})".format(sample.dst_mac))
+        if int(sample.dst_ip.split(".")[0]) >= 224 and int(sample.dst_ip.split(".")[0]) <= 239:
+            if sample.dst_mac[0:6] != "01005e":
+                return sFlowTestResult(True, "Dest MAC is not a valid multicast MAC", sample)
             else:
-                correct_mac = "01:00:5e:" + hex(sample.dst_ip[1] & 0x7F)[2:] + ":" + hex(sample.dst_ip[2])[2:] + ":" + hex(sample.dst_ip[3])[2:]
+                correct_mac = "01005e" + hex(int(sample.dst_ip.split(".")[1]) & 0x7F)[2:] + hex(int(sample.dst_ip.split(".")[2]))[2:] + hex(int(sample.dst_ip.split(".")[3]))[2:]
                 if sample.dst_mac != correct_mac:
-                    return sFlowTestResult(True, "Dest MAC does not match the multicast IP address ({})".format(sample.dst_mac))
-        
+                    return sFlowTestResult(True, "Dest MAC does not match the multicast IP address", sample)
+
         return sFlowTestResult(False)
 
 
@@ -58,24 +135,28 @@ if __name__ == "__main__":
     else:
         tests = sFlowTests()
         output_file = open("index.html", "w", 0)
-        sflow_file = open(sys.argv[0], 'r')
+        sflow_file = open(sys.argv[1], 'r')
         handler = SimpleHTTPServer.SimpleHTTPRequestHandler
         httpd = SocketServer.TCPServer(("", HTTP_PORT), handler)
         thread.start_new_thread(httpd.serve_forever, ())
-        print "* Starting HTTP server on port {}".format(HTTP_PORT)
-        output_file.write("<title>sFlow Packet Inspector</title>\n")
+        print "* Starting HTTP server on port {}\n".format(HTTP_PORT)
+        output_file.write('<head><title>sFlow Packet Inspector</title><meta http-equiv="refresh" content="10"></head>\n')
         try:
             while True:
                 new_line = sflow_file.readline()
-                if new_line:
+                if new_line and new_line[0:4] == "FLOW":
                     sample = sFlowSample(new_line)
-                    #result = tests.testBadMAC(sample)
-                    #if result.isError():
-                    #    output_file.write(result.getMessage() + "\n")
-                    #result = tests.testIncorrectMulticastMAC(sample)
-                    #if result.isError():
-                    #    output_file.write(result.getMessage() + "\n")
-                else:
+                    result = tests.testBadMAC(sample)
+                    if result.isError():
+                        print "ERROR: {}\n".format(result.getMessage())
+                        output_file.write("ERROR: {}<br /><br />\n".format(result.getMessage().replace("\n", "<br />\n")))
+                    result = tests.testIncorrectMulticastMAC(sample)
+                    if result.isError():
+                        print "ERROR: {}\n".format(result.getMessage())
+                        output_file.write("ERROR: {}<br /><br />\n".format(result.getMessage().replace("\n", "<br />\n")))
+                elif new_line and new_line[0:4] == "CNTR":
+                    sample = sFlowCounter(new_line)
+                elif not new_line:
                     time.sleep(1)
         except Exception:
             sflow_file.close()
